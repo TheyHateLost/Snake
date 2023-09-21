d9cd08cc1575899c93f008883b782b05fdbab10f
TheyHateLost <144147218+TheyHateLost@users.noreply.github.com>
2023-09-21 12:03:30 -0400

    Add files via upload

---
+++ snake script.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMove : MonoBehaviour
+{
+    //Variables 
+    private Vector2 direction; //control direction of movement 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //change direction of the snake 
+        if (input.GetKeyDown(KeyCode.W))  //when W key is pressed 
+        {
+            direction = Vector2.up;       //go up 
+        }    
+    }
+}
+       else if (input.GetKeyDown(KeyCode.S)) //when S key is pressed 
+{
+          direction = Vector2.down;      //go down 
+}
+
+       else if (input.GetKeyDown(KeyCode.A)) //when A key is pressed 
+{
+         direction = Vector2.left;       //go left 
+}  
+       else if (input.GetKeyDown(KeyCode.D)) //when D key is pressed 
+{
+         direction =  Vector2.right;     //go right 
+} 
+
+       //FixedUpdate is called at a fix interval 
+       void FixedUpdate()
+{
+        //move the snake 
+     this.transform.position = new Vector2(                     //get the position 
+         Mathf.Round(this.transform.position.x) + direction.x,  //round the number add value to x 
+         Mathf.Round(this.transform.position.x) + direction.y,  //round the number add value to y
+
+
+}
